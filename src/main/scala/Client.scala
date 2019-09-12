0f0da9e2136d83db76afd79c70e52c005457e40c
Anshika Agrawal <anshika.agrawal@knoldus.in>
2019-09-12 17:42:02 +0530

    try

--- src/main/scala/Client.scala
+++ src/main/scala/Client.scala
@@ -10,6 +10,7 @@ import scala.util.{Failure, Success}
 
   object Client {
     def main(args: Array[String]): Unit = {
+    
       implicit val system = ActorSystem()
       implicit val materializer = ActorMaterializer()
       // needed for the future flatMap/onComplete in the end
